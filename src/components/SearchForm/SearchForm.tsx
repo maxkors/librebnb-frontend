7564ffe52d606a93dbe053a08ae73d0831cc434a
Max <maxkorsv@gmail.com>
2022-10-22 17:20:58 +0300

    add place suggestion from Mapbox API to the Autocomplete

--- src/components/SearchForm/SearchForm.tsx
+++ src/components/SearchForm/SearchForm.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { SyntheticEvent, useState } from "react";
 import styles from "./SearchForm.module.scss";
 import { Autocomplete, Box, Button, TextField } from "@mui/material";
 import { DateRange, DateRangePicker } from "@mui/x-date-pickers-pro";
@@ -8,29 +8,84 @@ import { Dayjs } from "dayjs";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import GuestsMenu from "../../components/GuestsMenu";
 
+type Place = {
+	name: string;
+	bbox: number[];
+};
+
 const SearchForm = () => {
-	const [value, setValue] = React.useState<DateRange<Dayjs>>([null, null]);
-	const cities = ["London", "Munich", "Paris"];
+	const [inputValue, setInputValue] = useState<string>("");
+	const [places, setPlaces] = useState<Place[]>([]);
+	const [value, setValue] = useState<string>("");
+
+	const [date, setDate] = useState<DateRange<Dayjs>>([null, null]);
+
+	const getPlaces = async (input: string) => {
+		const response = await fetch(
+			`https://api.mapbox.com/geocoding/v5/mapbox.places/${input}.json?proximity=ip&types=place%2Ccountry%2Clocality&access_token=${process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}`
+		);
+		const data = await response.json();
+
+		const fetchedPlaces = data.features.map((feature: any) => ({
+			name: feature.place_name,
+			bbox: feature.bbox,
+		}));
+
+		console.log("fetchedPlaces: ");
+		console.log(fetchedPlaces);
+		return fetchedPlaces;
+	};
+
+	const onInputChange = (event: SyntheticEvent<Element, Event>, newInputValue: string) => {
+		setInputValue(newInputValue);
+		if (newInputValue !== "") {
+			getPlaces(newInputValue).then((places) => setPlaces(places));
+		}
+	};
+
+	const onChange = (event: SyntheticEvent<Element, Event>, newValue: string | null) => {
+		console.log("onChange: ");
+		console.log(newValue);
+		if (newValue !== null) {
+			setInputValue(newValue);
+			setValue(newValue);
+		}
+	};
 
 	return (
 		<div className={styles.searchMenu}>
 			<Autocomplete
 				className={styles.autocomplete}
-				options={cities}
+				inputValue={inputValue}
+				onInputChange={onInputChange}
+				value={value}
+				onChange={onChange}
+				options={places.map((place) => place.name)}
 				renderInput={(params) => <TextField {...params} label="Place" />}
 				popupIcon={null}
+				freeSolo
+				blurOnSelect
+				selectOnFocus
 			/>
 
 			<LocalizationProvider dateAdapter={AdapterDayjs} localeText={{ start: "Check-in", end: "Check-out" }}>
 				<DateRangePicker
-					value={value}
-					onChange={(newValue) => setValue(newValue)}
+					value={date}
+					onChange={(newDate) => {
+						console.log("newDate: ");
+						console.log(newDate);
+						setDate(newDate);
+					}}
+					onClose={() => {
+						// @ts-ignore
+						setTimeout(() => document.activeElement?.blur());
+					}}
 					renderInput={(startProps, endProps) => (
-						<React.Fragment>
+						<>
 							<TextField {...startProps} className={styles.dateRangePicker__field} />
 							<Box sx={{ mx: 0.4 }}></Box>
 							<TextField {...endProps} className={styles.dateRangePicker__field} />
-						</React.Fragment>
+						</>
 					)}
 				/>
 			</LocalizationProvider>
