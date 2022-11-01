08600c2e36f956578f712f993cd2c65fe041a686
Max <maxkorsv@gmail.com>
2022-11-01 12:23:51 +0300

    add toggle from from roomList to SearchMap

--- src/pages/SearchPage/SearchPage.tsx
+++ src/pages/SearchPage/SearchPage.tsx
@@ -4,6 +4,8 @@ import { useSearchParams } from "react-router-dom";
 import Header from "../../components/Header";
 import RoomCard from "../../components/RoomCard";
 import SearchMap from "../../components/SearchMap";
+import { Button } from "@mui/material";
+import { FormatListBulletedOutlined, MapOutlined } from "@mui/icons-material";
 
 export type Media = {
 	id: number;
@@ -29,6 +31,7 @@ export type BoundingBox = {
 const SearchPage = memo(() => {
 	const [searchParams, setSearchParams] = useSearchParams();
 	const [rooms, setRooms] = useState<Room[]>([]);
+	const [toggle, setToggle] = useState<boolean>(false);
 
 	const bbox: BoundingBox = {
 		SWLng: Number(searchParams.get("sw_lng") || -179.9),
@@ -54,22 +57,57 @@ const SearchPage = memo(() => {
 		});
 	}, [searchParams]);
 
+	const List = (
+		<div className={styles.roomList}>
+			{rooms.length > 0 && rooms.map((room, index) => <RoomCard room={room} key={index} />)}
+		</div>
+	);
+
+	const Map = (
+		<div className={styles.mapWrapper}>
+			<SearchMap
+				rooms={rooms}
+				SWLng={bbox.SWLng}
+				SWLat={bbox.SWLat}
+				NELng={bbox.NELng}
+				NELat={bbox.NELat}
+				setSearchParams={setSearchParams}
+			/>
+		</div>
+	);
+
 	return (
 		<div className={styles.searchPage}>
 			<Header />
-			<div className={styles.roomList}>
-				{rooms.length > 0 && rooms.map((room, index) => <RoomCard room={room} key={index} />)}
-			</div>
-			<div className={styles.mapWrapper}>
-				<SearchMap
-					rooms={rooms}
-					SWLng={bbox.SWLng}
-					SWLat={bbox.SWLat}
-					NELng={bbox.NELng}
-					NELat={bbox.NELat}
-					setSearchParams={setSearchParams}
-				/>
-			</div>
+			{window.innerWidth > 920 ? List : toggle ? null : List}
+			{window.innerWidth > 920 ? Map : toggle ? Map : null}
+			<Button
+				className={styles.toggle}
+				onClick={() => setToggle((prevState) => !prevState)}
+				variant="contained"
+				sx={{
+					position: "fixed",
+					bottom: "1.5rem",
+					left: "50%",
+					height: "2.8rem",
+					width: "6rem",
+					transform: "translate(-50%)",
+					zIndex: 10,
+					fontSize: "1rem",
+					textTransform: "none",
+					borderRadius: "20px",
+				}}
+			>
+				{toggle ? (
+					<span className={styles.toggle__content}>
+						List <FormatListBulletedOutlined />
+					</span>
+				) : (
+					<span className={styles.toggle__content}>
+						Map <MapOutlined />
+					</span>
+				)}
+			</Button>
 		</div>
 	);
 });
