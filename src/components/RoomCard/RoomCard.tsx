dcf1e65d62ca6e9cc76ad3087898e01c83e3562e
Max <maxkorsv@gmail.com>
2022-11-04 23:42:34 +0300

    add like button for RoomCard

--- src/components/RoomCard/RoomCard.tsx
+++ src/components/RoomCard/RoomCard.tsx
@@ -2,13 +2,18 @@ import styles from "./RoomCard.module.scss";
 import ImageGallery from "react-image-gallery";
 import { memo } from "react";
 import { Media, Room } from "../../pages/SearchPage";
+import FavoriteOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
 
 type Props = {
 	room: Room;
 };
 
 const RoomCard = memo(({ room }: Props) => (
-	<div className={styles.room}>
+	<div className={styles.roomCard}>
+		<FavoriteOutlinedIcon
+			className={styles.like}
+			sx={{ position: "absolute", zIndex: 10, color: "white", right: "10px", top: "10px", fontSize: "1.8rem" }}
+		/>
 		<div className={styles.gallery}>
 			<ImageGallery
 				items={room.media.map((media: Media) => ({
